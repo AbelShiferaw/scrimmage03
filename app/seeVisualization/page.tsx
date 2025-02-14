9a20b51a6969cf7a387910de7deb9611cb8485a2
Andrew <andrewle2003@gmail.com>
2025-02-14 15:11:04 -0500

    acc

--- app/seeVisualization/page.tsx
+++ app/seeVisualization/page.tsx
@@ -48,7 +48,7 @@ export default function SeeVisualizationPage() {
 
       <form onSubmit={handleSubmit2} className="w-full max-w-md mb-6 bg-white/10 p-6 rounded-lg shadow-md">
         <label htmlFor="input2" className="block text-sm font-bold mb-1">
-          Group Top 5
+          Enter Group
         </label>
         <input
           id="input2"
@@ -62,7 +62,7 @@ export default function SeeVisualizationPage() {
           type="submit"
           className="w-full bg-blue-500 hover:bg-blue-600 text-white p-3 rounded-lg text-lg font-bold cursor-pointer transition"
         >
-          Group All Traits
+          Submit Input 2
         </button>
       </form>
 
